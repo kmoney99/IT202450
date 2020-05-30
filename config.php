326a3e611f36abd1b1aa29b95c6b2f0e692c25ee
Kush Patel <47339310+kmoney99@users.noreply.github.com>
2020-05-29 23:21:24 -0400

    public_html

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+$cleardb_url      = parse_url(getenv("JAWSDB_URL"));
+$cleardb_server   = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db       = substr($cleardb_url["path"],1);
+?>
