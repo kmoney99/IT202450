e119ed51e7d27c9d9f4c71ca192f3771c73429c0
Kush Patel <47339310+kmoney99@users.noreply.github.com>
2020-05-30 17:54:47 -0400

    Delete config.php

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$cleardb_url      = parse_url(getenv("JAWSDB_URL"));
-$cleardb_server   = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db       = substr($cleardb_url["path"],1);
-?>
