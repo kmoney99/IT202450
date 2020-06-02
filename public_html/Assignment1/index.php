e91a90b1e745177b20c65963c5e732418eeff7bf
Kush Patel <47339310+kmoney99@users.noreply.github.com>
2020-06-02 15:33:41 -0400

    Update index.php

--- public_html/Assignment1/index.php
+++ public_html/Assignment1/index.php
@@ -3,7 +3,7 @@
   <!DOCTYPE HTML>
     <html>
      <body>
-<form action="/Assignment1" target="_self">
+<form>
   <label for="fname">First name:</label><br>
   <input type="text" id="fname" name="fname"><br>
   <label for="lname">Last name:</label><br>
