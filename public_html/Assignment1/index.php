ec3163bebd653efececeb8a711138fd708836df2
Kush Patel <47339310+kmoney99@users.noreply.github.com>
2020-06-02 15:35:41 -0400

    Update index.php

--- public_html/Assignment1/index.php
+++ public_html/Assignment1/index.php
@@ -1,17 +1,4 @@
 
 <?php
-  <!DOCTYPE HTML>
-    <html>
-     <body>
-<form>
-  <label for="fname">First name:</label><br>
-  <input type="text" id="fname" name="fname"><br>
-  <label for="lname">Last name:</label><br>
-  <input type="text" id="lname" name="lname">
-  <input type="submit" value="Submit">
-</form>
-    </body>
-    </html>
-    
-
+    echo "Hey,";
 ?>
