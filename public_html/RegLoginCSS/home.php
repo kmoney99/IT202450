a2a89c7098d9ed4eb4fbdb8699de500d2b475a57
Kush Patel <kp627@njit.edu>
2020-06-12 15:43:09 -0400

    update

--- public_html/RegLoginCSS/home.php
+++ public_html/RegLoginCSS/home.php
@@ -2,5 +2,7 @@
 include("header.php");
 
 ?>
+<div class="center">
 <h1 style="background-color:DodgerBlue;">Welcome to my homepage, Greetings from Kush!</h1>
+</div>
 <?php $_SESSION["user"]["email"];?>
