d983195cb1d01006e17c31b24e074577e6d05db8
Kush Patel <kp627@njit.edu>
2020-06-14 16:59:05 -0400

    Login update

--- public_html/RegLoginCSS/login.php
+++ public_html/RegLoginCSS/login.php
@@ -23,17 +23,8 @@ include("header.php");
 <?php
 
 
-if(isset($_POST["login"])){
-	
-$connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
-			
-				$db = new PDO($connection_string, $dbuser, $dbpass);
-				$stmt = $db->prepare("SELECT * FROM Users where email = :email LIMIT 1");
-				$stmt->execute(array(
-					":email" => $email
-				));
-				
-				$fields = array('Email','Password');
+<?php
+$fields = array('Email','Password');
 
 				foreach($fields AS $fieldname) { //Looping through each type in fields 
 				  if(!isset($_POST[$fieldname]) || empty($_POST[$fieldname])) { //Checking if none of them are empty
@@ -42,6 +33,18 @@ $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
 					 
 				  }
 				}
+if(isset($_POST["login"])){
+	if(isset($_POST["password"]) && isset($_POST["email"])){
+		$password = $_POST["password"];
+		$email = $_POST["email"];
+		require("config.php");
+			$connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
+			try{
+				$db = new PDO($connection_string, $dbuser, $dbpass);
+				$stmt = $db->prepare("SELECT * FROM Users where email = :email LIMIT 1");
+				$stmt->execute(array(
+					":email" => $email
+				));
 				$e = $stmt->errorInfo();
 				if($e[0] != "00000"){
 					echo var_export($e, true);
@@ -51,10 +54,14 @@ $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
 					if ($result){
 						$rpassword = $result["password"];
 						if(password_verify($password, $rpassword)){
-							
+							echo "<div>Passwords matched! You are technically logged in!</div>";
 							$_SESSION["user"] = array(
+								"id"=>$result["id"],
 								"email"=>$result["email"],
-								"password"=>$result["rpassword"]
+								"first_name"=>$result["first_name"],
+								"last_name"=>$result["last_name"]
+								 
+								 
 							);
 							echo var_export($_SESSION, true);
 							header("Location: home.php");
@@ -66,8 +73,12 @@ $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
 					else{
 						echo "<div>Invalid user</div>";
 					}
-				
+					//echo "<div>Successfully registered!</div>";
 				}
-
+			}
+			catch (Exception $e){
+				echo $e->getMessage();
+			}
+	}
 }
 ?>
