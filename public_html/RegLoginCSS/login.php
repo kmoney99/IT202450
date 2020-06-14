81d5d608b424162eb2286fff634c48842e64bb02
Kush Patel <kp627@njit.edu>
2020-06-14 12:39:23 -0400

    Login update

--- public_html/RegLoginCSS/login.php
+++ public_html/RegLoginCSS/login.php
@@ -34,31 +34,43 @@ foreach($fields AS $fieldname) { //Loop trough each field
 }
 	
 }
-
-$email = $_POST['eamil'];
-$password = $_POST['password'];
-
-$sql = "SELECT password
-        FROM members
-        WHERE email = '$email'		
-      ";
-	  
-	  $result = mysqli_query( $conn, $sql);
-	  
-	  while ($row = mysqli_fetch_assoc($result ) ) {
-		
-		$pw = $row['password'];
-	  }
-		
-	if ($pw == $password) {
-          
-		session_start();
-		$_SESSION['u_e'] = $email;
-		header("Location: home.php");
- 
-       		
-	  }
-
-
-
+$connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
+			try{
+				$db = new PDO($connection_string, $dbuser, $dbpass);
+				$stmt = $db->prepare("SELECT * FROM Users where email = :email LIMIT 1");
+				$stmt->execute(array(
+					":email" => $email
+				));
+				$e = $stmt->errorInfo();
+				if($e[0] != "00000"){
+					echo var_export($e, true);
+				}
+				else{
+					$result = $stmt->fetch(PDO::FETCH_ASSOC);
+					if ($result){
+						$rpassword = $result["password"];
+						if(password_verify($password, $rpassword)){
+							echo "<div>Passwords matched! You are technically logged in!</div>";
+							$_SESSION["user"] = array(
+								"id"=>$result["id"],
+								"email"=>$result["email"],
+								"first_name"=>$result["first_name"],
+								"last_name"=>$result["last_name"]
+							);
+							echo var_export($_SESSION, true);
+							header("Location: home.php");
+						}
+						else{
+							echo "<div>Invalid password!</div>";
+						}
+					}
+					else{
+						echo "<div>Invalid user</div>";
+					}
+					//echo "<div>Successfully registered!</div>";
+				}
+			}
+			catch (Exception $e){
+				echo $e->getMessage();
+			}
 ?>
