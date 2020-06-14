bec90cdb2da8834e37b130cc5d46560c741ab66c
Kush Patel <kp627@njit.edu>
2020-06-14 17:45:11 -0400

    Login update

--- public_html/RegLoginCSS/login.php
+++ public_html/RegLoginCSS/login.php
@@ -22,12 +22,15 @@ include("header.php");
 
 
 <?php
+
+//echo var_export($_GET, true);
+//echo var_export($_POST, true);
+//echo var_export($_REQUEST, true);
 if(isset($_POST["login"])){
 	if(isset($_POST["password"]) && isset($_POST["email"])){
 		$password = $_POST["password"];
 		$email = $_POST["email"];
-	
-		require("config.php");
+		//require("config.php");
 			$connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
 			try{
 				$db = new PDO($connection_string, $dbuser, $dbpass);
@@ -50,8 +53,6 @@ if(isset($_POST["login"])){
 								"email"=>$result["email"],
 								"first_name"=>$result["first_name"],
 								"last_name"=>$result["last_name"]
-								 
-								 
 							);
 							echo var_export($_SESSION, true);
 							header("Location: home.php");
