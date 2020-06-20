623457619c7f389c297ce634f04fe4f0bb9cbd45
Kush Patel <kp627@njit.edu>
2020-06-20 14:42:03 -0400

    Week 5 HW

--- public_html/RegLoginCSS/table_survey.sql
+++ public_html/RegLoginCSS/table_survey.sql
@@ -1,5 +1,5 @@
-CREATE TABLE question ( 
-	id int auto_increment, 
+CREATE TABLE survey ( 
+	question_id int auto_increment, 
 	title varchar(30) not null unique,
 	description TEXT,
     primary key(id)
