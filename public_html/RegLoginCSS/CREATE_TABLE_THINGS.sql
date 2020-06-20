d55d905de1e0f546cb3d672f339f8c4c839555e8
Kush Patel <kp627@njit.edu>
2020-06-20 14:49:53 -0400

    Update files

--- public_html/RegLoginCSS/CREATE_TABLE_THINGS.sql
+++ public_html/RegLoginCSS/CREATE_TABLE_THINGS.sql
@@ -5,4 +5,5 @@ CREATE TABLE Things (
     created datetime default current_timestamp,
     modified datetime default current_timestamp onupdate current_timestamp,
     primary key (id)
+	
 )
--- public_html/RegLoginCSS/table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE question ( 
-	id int auto_increment, 
-	title varchar(30) not null unique,
-	description TEXT,
-    primary key(id)
-	
-
-);
-
