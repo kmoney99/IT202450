e26b1b380cf3946c26582b73b2fbeeb8d7035af9
Kush Patel <kp627@njit.edu>
2020-06-20 20:57:23 -0400

    Week 5 Update

--- public_html/Week5HW/create_survey.php
+++ public_html/Week5HW/create_survey.php
@@ -4,11 +4,17 @@
 
 <form method="POST">
 <label for="Question Cat">Pick a question category:
-  <select id="Category" name="Cat">
+  <select id="Category" name="title">
   <option value="sp">Sports</option>
   <option value="co">Countries</option>
   <option value="in">World Wide Web</option>
 </label>
   
   </select>
+  
+ <label for="num">Enter your Question:
+	<input type="text" id="d" name="description" />
+	</label>
+	<input type="submit" name="created" value="Create Question"/>
+	
 </form>
