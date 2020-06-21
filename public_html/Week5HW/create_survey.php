721530861ea4b609f1dc48be97dce592700ae77d
Kush Patel <kp627@njit.edu>
2020-06-20 20:51:00 -0400

    Week 5 Update

--- public_html/Week5HW/create_survey.php
+++ public_html/Week5HW/create_survey.php
@@ -1,48 +1,14 @@
+
+<link rel="stylesheet" type="text/css" href="style.css">
+
+
 <form method="POST">
-	<label for="thing">Category Name:
-	<input type="text" id="thing" name="name" />
-	</label>
-	<label for="q">Number of Questions:
-	<input type="number" id="q" name="quantity" />
-	</label>
-	<input type="submit" name="created" value="Create Question"/>
+<label for="Question Cat">Pick a question category:
+  <select id="Category" name="Cat">
+  <option value="sp">Sports</option>
+  <option value="co">Countries</option>
+  <option value="in">World Wide Web</option>
+</label>
+  
+  </select>
 </form>
-
-
-<?php
-if(isset($_POST["created"])){
-    $name = $_POST["name"];
-    $quantity = $_POST["quantity"];
-    if(!empty($name) && !empty($quantity)){
-        require("config.php");
-        $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
-        try{
-            $db = new PDO($connection_string, $dbuser, $dbpass);
-            $stmt = $db->prepare("INSERT INTO Things (name, quantity) VALUES (:name, :quantity)");
-            $result = $stmt->execute(array(
-                ":name" => $name,
-                ":quantity" => $quantity
-            ));
-            $e = $stmt->errorInfo();
-            if($e[0] != "00000"){
-                echo var_export($e, true);
-            }
-            else{
-                echo var_export($result, true);
-                if ($result){
-                    echo "Successfully inserted new thing: " . $name;
-                }
-                else{
-                    echo "Error inserting record";
-                }
-            }
-        }
-        catch (Exception $e){
-            echo $e->getMessage();
-        }
-    }
-    else{
-        echo "Name and quantity must not be empty.";
-    }
-}
-?>
