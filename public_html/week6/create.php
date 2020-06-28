7e65488a9553ebc84dd8d520813edb98b86667d2
Kush Patel <kp627@njit.edu>
2020-06-28 17:00:35 -0400

    Week 6 Post

--- public_html/week6/create.php
+++ public_html/week6/create.php
@@ -14,6 +14,9 @@
  <label for="num">Enter your Question:
 	<input type="text" id="d" name="description" />
 	</label>
+	<label for="num">Enter your Question Number:
+	<input type="number" id="d" name="number" />
+	</label>
 	<input type="submit" name="created" value="Create Question"/>
 	
 </form>
