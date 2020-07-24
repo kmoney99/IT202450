9a0200d1c8311a8ab67ac5f54832e82626038b01
Kush Patel <kp627@njit.edu>
2020-07-23 20:09:19 -0400

    Typo Fix

--- public_html/Project/partials/header.partial.php
+++ public_html/Project/partials/header.partial.php
@@ -13,9 +13,36 @@ $logged_in = Common::is_logged_in(false);
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <ul class="navbar-nav mr-auto">
-	 <li class="nav-item">
+        <?php if($logged_in):?>
+        <li class="nav-item">
+            <a class="nav-link" href="<?php echo Common::url_for("home");?>">Home</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="<?php echo Common::url_for("game");?>">Game</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="<?php echo Common::url_for("shop");?>">Shop</a>
+        </li>
+        <?php if (Common::has_role("Admin")):?>
+        <li class="nav-item">
+            <a class="nav-link" href="<?php echo Common::url_for("create_questionnaire");?>">Create Questionnaire</a>
+        </li>
+        <?php endif;?>
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="compDropdown" role="button"
+               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Competitions
+            </a>
+            <div class="dropdown-menu" aria-labelledby="compDropdown">
+                <a class="dropdown-item" href="<?php echo Common::url_for("create_competition");?>">Create</a>
+                <a class="dropdown-item" href="<?php echo Common::url_for("competitions");?>">Available</a>
+                <a class="dropdown-item" href="<?php echo Common::url_for("competitions");?>?status=registered">Registered</a>
+            </div>
+        </li>
+        <li class="nav-item">
             <a class="nav-link" href="<?php echo Common::url_for("surveys");?>">Surveys</a>
         </li>
+        <?php endif; ?>
         <?php if(!$logged_in):?>
             <li class="nav-item">
                 <a class="nav-link" href="<?php echo Common::url_for("login");?>">Login</a>
