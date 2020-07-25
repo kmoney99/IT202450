7cf0da8bba0efa6eda741d9a4358cc860a9c145c
Kush Patel <kp627@njit.edu>
2020-07-25 19:58:22 -0400

    Improved register screen

--- public_html/Project/register.php
+++ public_html/Project/register.php
@@ -16,8 +16,19 @@ include_once(__DIR__."/partials/header.partial.php");
 	" href="/Project/includes/../login.php">Login</a>
             </li>
             <li class="nav-item" style="margin-left: 10px;">
-               
+                <a style="
+    color: black;
+    font-weight: bold;
+" class="nav-link" href="/Project/includes/../register.php">Register</a>
+            </li>
+            </ul>
 </nav>
+<div id="messages">
+        </div>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+<!-- Include jQuery 3.5.1-->
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+
 <div id="messages">
         </div>    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 <!-- Include jQuery 3.5.1-->
@@ -88,8 +99,9 @@ include_once(__DIR__."/partials/header.partial.php");
         
 </div></form></div>     
              
-</body></html>    
+    
              
+</body></html>
 <?php
 if (Common::get($_POST, "submit", false)){
     $email = Common::get($_POST, "email", false);
