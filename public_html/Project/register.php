b5906bb91721fd701fd180d7061490dd64b1d6d9
Kush Patel <kp627@njit.edu>
2020-07-24 21:50:59 -0400

    Updated Files

--- public_html/Project/register.php
+++ public_html/Project/register.php
@@ -1,46 +1,86 @@
+<?php include("header.php");?>
+
+<link rel="stylesheet" type="text/css" href="style.css">
+
+<h1 style="background-color:Tomato;">Register</h1>
+
+<form method="POST">
+
+ <div style='text-align:center'>
+
+	<label for="email">Email:<font color=red>*</font>
+	<input type="email" id="email" name="email" autocomplete="off" />
+	</label>
+
+	<label for="p">Password:<font color=red>*</font>
+	<input type="password" id="p" name="password" autocomplete="off"/>
+	</label>
+
+	<label for="cp">Confirm Password:<font color=red>*</font>
+	<input type="password" id="cp" name="cpassword"/>
+	</label>
+
+	<input type="submit" name="register" value="Register"/>
+
+</form>
+
+
+
 <?php
-include_once(__DIR__."/partials/header.partial.php");
-?>
-    <div>
-        <h4>Register</h4>
-        <form method="POST">
-            <div>
-                <label for="email">Email</label>
-                <input type="email" id="email" name="email" required/>
-            </div>
-            <div>
-                <label for="password">Password</label>
-                <input type="password" id="password" name="password" required min="3"/>
-            </div>
-            <div>
-                <label for="cpassword">Confirm Password</label>
-                <input type="password" id="cpassword" name="cpassword" required min="3"/>
-            </div>
-            <input type="submit" name="submit" value="Register"/>
-        </form>
-    </div>
-<?php
-if (Common::get($_POST, "submit", false)){
-    $email = Common::get($_POST, "email", false);
-    $password = Common::get($_POST, "password", false);
-    $confirm_password = Common::get($_POST, "cpassword", false);
-    if($password != $confirm_password){
-        Common::flash("Passwords must match", "warning");
-        die(header("Location: register.php"));
-    }
-    if(!empty($email) && !empty($password)){
-        $result = DBH::register($email, $password);
-        echo var_export($result, true);
-        if(Common::get($result, "status", 400) == 200){
-            //Note to self: Intentionally didn't add tank creation here
-            //keeping it in login where it is (creates a new tank only if user has no tanks)
-            //it fulfills the purpose there
-            Common::flash("Successfully registered, please login", "success");
-            die(header("Location: " . Common::url_for("login")));
-        }
-    }
-    else{
-        Common::flash("Email and password must not be empty", "warning");
-        die(header("Location: register.php"));
-    }
+
+if(isset($_POST["register"])){
+	if(isset($_POST["password"]) && isset($_POST["cpassword"]) && isset($_POST["email"])){
+		$password = $_POST["password"];
+		$cpassword = $_POST["cpassword"];
+		$email = $_POST["email"];
+	
+		if($password == $cpassword){
+			$connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
+			try{
+				$db = new PDO($connection_string, $dbuser, $dbpass);
+				$hash = password_hash($password, PASSWORD_BCRYPT);
+				$stmt = $db->prepare("INSERT INTO Users (email, password) VALUES(:email, :password)");
+				$stmt->execute(array(
+					":email" => $email,
+					":password" => $hash//Don't save the raw password $password
+				));
+				 echo "<br>";
+				if (empty($_POST['email'])) {
+		
+					echo "Email cannot be empty";
+					
+				}
+				 echo "<br>";
+				if (empty($_POST['password'])) {
+		
+					echo "Password cannot be empty";
+		
+				}
+				 echo "<br>";
+				if (empty($_POST['cpassword'])) {
+		
+					echo "Confirm password cannot be empty";
+		
+				}
+				
+				
+				
+				$e = $stmt->errorInfo();
+				if($e[0] != "00000"){
+				}
+				else{
+					echo "<div>Successfully registered!</div>";
+					
+				}
+				
+			}
+			catch (Exception $e){
+				echo $e->getMessage();
+			}
+		}
+		else{
+			echo "<div> Passwords don't match </div>";
+		}
+	}
 }
+?>
