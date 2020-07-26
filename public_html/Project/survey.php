6fa57c2c502bb02596c247096c302e0550855ee1
Kush Patel <kp627@njit.edu>
2020-07-26 18:03:19 -0400

    Updated survey.php

--- public_html/Project/survey.php
+++ public_html/Project/survey.php
@@ -11,7 +11,6 @@ else{
     Common::flash("Not a valid survey", "warning");
     die(header("Location: surveys.php"));
 }
-
 if(!Common::get($_POST, "submit", false)){
     $response = DBH::check_survey_status($questionnaire_id);
     if(Common::get($response, "status", 400) == 200){
@@ -22,8 +21,22 @@ if(!Common::get($_POST, "submit", false)){
         $total = (int)Common::get($data, "responses_total", 0);
         $max = (int)Common::get($data, "max_attempts", 0);
         $apd = (int)Common::get($data, "attempts_per_day", 0);
-	}
-
+        if($use_max){
+            //TODO check query as this part doesn't seem accurate with the group by
+            //but the group by is needed for the "today" check
+            if($total >= $max){
+                Common::flash("Max responses have already been recorded for this survey", "warning");
+                die(header("Location: surveys.php"));
+            }
+        }
+        else{
+            if($today >= $apd){
+                Common::flash("Max responses have already been recorded today for this survey", "warning");
+                die(header("Location: surveys.php"));
+            }
+        }
+    }
+}
 //TODO: Note, internally calling them questionnaires (and for admin), user facing they're called surveys.
 $response = DBH::get_questionnaire_by_id($questionnaire_id);
 $available = [];
@@ -40,7 +53,12 @@ if(Common::get($_POST, "submit", false)){
         if(strpos($key, "question") !== false) {
             $is_other = false;
             $question_id = (int)explode("-", $key)[1];
-            
+            if (strpos($key, "other") !== false) {
+                if (trim(strlen($value)) > 0) {
+                    $is_other = true;
+                    $answer_id = (int)explode("-", $key)[3];
+                    array_push($response, ["question_id" => $question_id, "answer_id" => $answer_id, "user_input" => $value]);
+                }
             }
             else{
                 array_push($response, ["question_id" => $question_id, "answer_id" => $value]);
@@ -50,13 +68,22 @@ if(Common::get($_POST, "submit", false)){
     //echo "<br><pre>" . var_export($response, true) . "</pre>";
     if(count($response) > 0){
         $response = DBH::save_response($questionnaire_id, $response);
-       
+        if(Common::get($response, "status", 400) == 200){
+            Common::flash("Successfully recorded response", "success");
+            $result = DBH::changePoints(Common::get_user_id(), 1, -1, "survey", "Thanks for taking this survey");
+            if(Common::get($result, "status", 400) == 200) {
+                Common::flash("You earned 1 point, thanks for participating!", "success");
+            }
+        }
+        else{
+            Common::flash("Error recording response", "danger");
+        }
     }
     else{
         Common::flash("Error recording response", "danger");
     }
     die(header("Location: surveys.php"));
-
+}
 ?>
 <div class="container-fluid">
     <!-- see https://www.w3schools.com/php/func_array_reset.asp for use of current() function -->
