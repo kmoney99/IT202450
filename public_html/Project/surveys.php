4b8d82bbbe5671ee0dbe2b54b10ee0cb9c608ca3
Kush Patel <kp627@njit.edu>
2020-07-26 16:20:43 -0400

    Added Design on Survey.php

--- public_html/Project/surveys.php
+++ public_html/Project/surveys.php
@@ -11,20 +11,97 @@ if(Common::get($response, "status", 400) == 200){
     $available = Common::get($response, "data", []);
 }
 ?>
-<div class="container-fluid">
-    <h4>Surveys</h4>
+
+	<html>
+    <ul class="navbar-nav mr-auto" style="margin: 0 auto;">
+                    <li class="nav-item">
+                <a class="nav-link" style="
+    color: black;
+    font-weight: bold;
+	margin: 10px;
+	" href="/Project/includes/../home.php">Home</a>
+            </li>
+			<li class="nav-item">
+                <a class="nav-link" style="
+    color: black;
+    font-weight: bold;
+	margin: 10px;
+	" href="/Project/includes/../search.php">Search</a>
+            </li>
+                <li class="nav-item">
+                    <a class="nav-link" style="
+    color: black;
+    font-weight: bold;
+	margin: 10px;
+	" href="/Project/includes/../create_questionnaire.php">Create Custom Survey</a>
+                </li>
+				
+            <li class="nav-item">
+                <a class="nav-link" style="
+    color: black;
+    font-weight: bold;
+	margin: 10px;
+	" href="/Project/includes/../surveys.php">Surveys</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" style="
+    color: black;
+    font-weight: bold;
+	margin: 10px;
+	" href="/Project/includes/../TakenSurveys.php">Surveys you've Taken</a>
+            </li>
+                            <li class="nav-item">
+                <a class="nav-link" style="
+    color: black;
+    font-weight: bold;
+	margin: 10px;
+	" href="/Project/includes/../logout.php">Logout</a>
+            </li>
+            </ul>
+</nav>
+<div id="messages">
+        </div><div class="container-fluid">
+    <h4 style="
+    margin: auto 0;
+    text-align: center;
+    padding-top: 4rem;
+">Surveys</h4>
     <div class="list-group">
-        <?php foreach($available as $s): ?>
-            <div class="list-group-item">
-                <h6><?php echo Common::get($s, "name", ""); ?></h6>
-                <p><?php echo Common::get($s, "description", ""); ?></p>
-                <?php if(Common::get($s, "use_max", false)): ?>
-                    <div>Max Attempts: <?php echo Common::get($s, "max_attempts", 0);?></div>
-                <?php else:?>
-                    <div>Daily Attempts: <?php echo Common::get($s, "attempts_per_day", 0);?></div>
-                <?php endif; ?>
-                <a href="survey.php?s=<?php echo Common::get($s, 'id', -1);?>" class="btn btn-secondary">Participate</a>
+                    <div class="list-group-item" style="
+    text-align: center;
+">
+                <h6>Phones</h6>
+                <p>Phones mostly used</p>
+                                    <div>Daily Attempts: 1</div>
+                                <a href="survey.php?s=1" class="btn btn-secondary">Participate</a>
             </div>
+                    <div class="list-group-item" style="
+    text-align: center;
+">
+                <h6>Sports</h6>
+                <p>GK on FIFA</p>
+                                    <div>Daily Attempts: 1</div>
+                                <a href="survey.php?s=2" class="btn btn-secondary">Participate</a>
+            </div>
+                    </div>
+</div></body></html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         <?php endforeach; ?>
         <?php if(count($available) == 0):?>
             <div class="list-group-item">
