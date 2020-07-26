d957ccde19ee5a8e54b62c6d4f286635fd7ddd32
Kush Patel <kp627@njit.edu>
2020-07-25 20:24:52 -0400

    Improved register screen

--- public_html/Project/register.php
+++ public_html/Project/register.php
@@ -2,8 +2,11 @@
 include_once(__DIR__."/partials/header.partial.php");
 ?>
     <html><head><link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-
-
+<!-- Include jQuery 3.5.1-->
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+</head><body data-gr-c-s-loaded="true" style="
+    background: #28334Aff;
+">
         
 <li class="nav-item">
                 <a class="nav-link" style="
