4d244eeae28fb7332311f789909e43caea61b1d6
Kush Patel <kp627@njit.edu>
2020-07-26 14:09:19 -0400

    fixing surveys

--- public_html/Project/surveys.php
+++ public_html/Project/surveys.php
@@ -23,7 +23,7 @@ if(Common::get($response, "status", 400) == 200){
                 <?php else:?>
                     <div>Daily Attempts: <?php echo Common::get($s, "attempts_per_day", 0);?></div>
                 <?php endif; ?>
-                <a href="survey.php?s=<?php echo Common::get($s, 'id', -1);?>" class="btn btn-secondary">Participate</a>
+                <a href="survey.php?s=<?php echo Common::get($s, 'id', -3);?>" class="btn btn-secondary">Participate</a>
             </div>
         <?php endforeach; ?>
         <?php if(count($available) == 0):?>
