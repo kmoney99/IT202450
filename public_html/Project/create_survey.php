4a0dcbebdb21a228b9f131aac9e56ca9d56eded0
Kush Patel <kp627@njit.edu>
2020-07-29 15:15:55 -0400

    Minior changes

--- public_html/Project/create_survey.php
+++ public_html/Project/create_survey.php
@@ -60,8 +60,8 @@ if(isset($_POST["created"])) {
     }
 
     try {
-        
-        $query = file_get_contents(__DIR__ . "/sql/queries/INSERT_TABLE_SURVEY.sql");
+        require("includes/common.inc.php");
+        $query = file_get_contents(__DIR__ . "queries/INSERT_TABLE_SURVEY.sql");
         if(isset($query) && !empty($query)) {
             $stmt = getDB()->prepare($query);
             $result = $stmt->execute(array(
