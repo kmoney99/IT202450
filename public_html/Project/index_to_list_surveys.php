256052894d98116f104e9a04dc2b424da1bf3543
Kush Patel <kp627@njit.edu>
2020-07-31 19:24:49 -0400

    Pushed list_surveys working

---
+++ public_html/Project/index_to_list_surveys.php
@@ -0,0 +1,6 @@
+<?php
+include_once(__DIR__."/partials/header.partial.php");
+
+header("Location: /week6/list.php");
+
+?>
--- public_html/Project/list_surveys.php
+++
@@ -1,8 +0,0 @@
-<?php
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
-
-include("header.php");
-header("Location: /week6/list.php");
-
-?>
