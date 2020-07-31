3eda6c4290b3934b8537e9fb010689a7a31ab061
Kush Patel <kp627@njit.edu>
2020-07-31 14:53:53 -0400

    Push title to list_survey

--- public_html/Project/list_surveys.php
+++ public_html/Project/list_surveys.php
@@ -4,16 +4,28 @@ ini_set('display_errors', '1');
 
 include_once(__DIR__."/partials/header.partial.php");
 
- $sql = "SELECT * FROM SURVEY;";
- $result = mysqli_query($connection_string, $sql);
- $result_check = mysqli_num_rows($result);
- 
- if ($result_check > 0) {
-	 while ($row = mysqli_fetch_assoc($result)) {
-		 echo $row['title'];
-	 
- }
- 
- }
-
+require("config.php");
+			
+			$common->getDB()->prepare("SELECT * FROM SURVEY where title = :title LIMIT 1");
+				
+			$stmt->execute(array(
+				":title" => $title
+			));
+			
+			$result = $stmt->execute(array(
+                ":title" => $title,
+			));
+				
+			$e = $stmt->errorInfo();
+				if($e[0] != "00000"){
+					echo var_export($e, true);
+				}
+				else{
+					$result = $stmt->fetch(PDO::FETCH_ASSOC);
+					if ($result){
+						echo "Survey" . $title; 
+					}
+				}
+		
+						
 ?>
