470a09ce9ecaef86b762bcf58a17b5700b1a2cc3
Kush Patel <kp627@njit.edu>
2020-07-31 19:17:30 -0400

    Minor Changes

--- public_html/Project/list_surveys.php
+++ public_html/Project/list_surveys.php
@@ -2,38 +2,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
+include("header.php");
+header("Location: /week6/list.php");
 
-include_once(__DIR__."/partials/header.partial.php");
-
-
-require("common.inc.php"(__DIR__."/includes/common.inc.php"));
-$query = file_get_contents(__DIR__ . "/queries/SELECT_ALL_TABLE_SURVEY.sql");
-if(isset($query) && !empty($query)){
-    try {
-        $stmt = getDB()->prepare($query);
- 
-        $stmt->execute();
-  
-        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    }
-    catch (Exception $e){
-        echo $e->getMessage();
-    }
-}
 ?>
-
-<?php if(isset($results)):?>
-    <p>Surveys</p>
-    <ul>
-        
-        <?php foreach($results as $row):?>
-            <li>
-                <?php echo get($row, "title")?>
-                <?php echo get($row, "description");?>
-                <a href="delete.php?surveyID=<?php echo get($row, "id");?>">Delete</a>
-            </li>
-        <?php endforeach;?>
-    </ul>
-<?php else:?>
-    <p>No Surveys is avaliable. </p>
-<?php endif;?>
