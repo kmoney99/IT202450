e36c06ffc162a1b66d64480d432b57618ce16c90
Kush Patel <kp627@njit.edu>
2020-07-31 19:51:14 -0400

    added header

--- public_html/list_surveys/list.php
+++ public_html/list_surveys/list.php
@@ -1,4 +1,6 @@
 <?php
+
+include_once(__DIR__."/partials/header.partial.php");
 require("common.inc.php");
 $query = file_get_contents(__DIR__ . "/queries/SELECT_ALL_TABLE_SURVEY.sql");
 if(isset($query) && !empty($query)){
