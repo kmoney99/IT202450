b1bb8651a6df3cac3da2e400fef5ce5e3d3438b0
Kush Patel <kp627@njit.edu>
2020-07-31 16:33:46 -0400

    fixed an error

--- public_html/Project/list_surveys.php
+++ public_html/Project/list_surveys.php
@@ -19,19 +19,12 @@ include_once(__DIR__."/partials/header.partial.php");
         $query = file_get_contents(__DIR__ . "/sql/queries/INSERT_TABLE_SURVEY.sql");
         if(isset($query) && !empty($query)) {
 			
-			$sql = "SELECT title from SURVEY (title) values (:title)";
+			$common->getDB()->prepare("SELECT title from SURVEY");
 			
-			$stmt = $common->getDB()->prepare ($sql);
-			
-			$stmt -> execute ([":title" =>$title]);
+			$stmt = $common->getDB()->prepare ($common);
             
 			$stmt = $common->getDB()->prepare($query);
 			
-            
-			$result = $stmt->execute(array(
-			    ":title" => $title,
-            
-			));
 			$e = $stmt->errorInfo();
 				if($e[0] != "00000"){
 					echo var_export($e, true);
