97b70332c8b76e85a5394e01b678185b952b0609
Kush Patel <kp627@njit.edu>
2020-08-01 13:16:29 -0400

    fixed list_surveys

--- public_html/Project/list_surveys.php
+++ public_html/Project/list_surveys.php
@@ -1,39 +1,5 @@
 <?php
+header("Location: /list_surveys/list.php");
+exit();
 
-include_once(__DIR__."/partials/header.partial.php");
-require("com.inc.php");
-$query = file_get_contents(__DIR__ . "/sql/queries/SELECT_ALL_TABLE_SURVEY.sql");
-if(isset($query) && !empty($query)){
-    try {
-        $stmt = getDB()->prepare($query);
-       
-        $stmt->execute();
-        
-        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    }
-    catch (Exception $e){
-        echo $e->getMessage();
-    }
-}
 ?>
-
-<?php if(isset($results)):?>
-    <p>Surveys By Title</p>
-    <ul>
-        
-        <?php foreach($results as $row):?>
-            <li>
-                <?php echo get($row, "title")?>
-				
-				<a href="add_question.php?surveyID=<?php echo get($row, "id");?>">Add Question</a>
-				
-                <a href="delete.php?surveyID=<?php echo get($row, "id");?>">Delete Survey</a>
-				
-				<a href="view_survey.php?surveyID=<?php echo get($row, "id");?>">View Survey</a>
-				
-            </li>
-        <?php endforeach;?>
-    </ul>
-<?php else:?>
-    <p>No Surveys at this time.</p>
-<?php endif;?>
