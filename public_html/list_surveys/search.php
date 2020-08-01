aa057f83da70ec535250c65156d328a1bd0d6dcb
Kush Patel <kp627@njit.edu>
2020-08-01 16:19:36 -0400

    fixed minor errors/push search

---
+++ public_html/list_surveys/search.php
@@ -0,0 +1,54 @@
+<?php
+$search = "";
+if(isset($_POST["search"])){
+    $search = $_POST["search"];
+}
+?>
+<link rel="stylesheet" type="text/css" href="sty.css">
+<h3> Search for survey question </h3>
+<form method="POST">
+<label for="Sort the Data">Sort the Surveys:
+  <select id="Sort" name="title">
+ <option value= query "SELECT * FROM `Survey` ORDER BY `title` ASC">Ascending Sort</option>
+ 
+ <option value= query "SELECT * FROM `Survey`  ORDER BY `title` DESC">Descending Sort</option>
+
+    <input type="text" name="search" placeholder="Search.." value="<?php echo $search;?>"/> 
+    <input type="submit" value="Search"/>
+
+</form>
+
+
+<?php
+if(isset($search)) {
+
+    require("common.inc.php");
+    $query = file_get_contents(__DIR__ . "/queries/SEARCH_TABLE_SURVEY.sql");
+    if (isset($query) && !empty($query)) {
+        try {
+            $stmt = getDB()->prepare($query);
+  
+            $stmt->execute([":survey"=>$search]);
+            
+            $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+    }
+}
+?>
+
+<?php if(isset($results) && count($results) > 0):?>
+    <p>Results</p>
+    <ul>
+        
+        <?php foreach($results as $row):?>
+            <li>
+                <?php echo get($row, "title")?>
+                
+            </li>
+        <?php endforeach;?>
+    </ul>
+<?php else:?>
+    <p>Results</p>
+<?php endif;?>
