87d4a50e447e0c773c7a2fb7e7cc3ba3da73cea6
Kush Patel <kp627@njit.edu>
2020-08-01 19:36:39 -0400

    fixed inc.php/add_question

--- public_html/list_surveys/add_question.php
+++ public_html/list_surveys/add_question.php
@@ -40,7 +40,6 @@ ini_set('display_errors', '1');
 
 
 <?php
-require("kush.inc.php");
 $query = file_get_contents(__DIR__ . "/sql/queries/SELECT_ALL_TABLE_SURVEY.sql");
 if(isset($query) && !empty($query)){
     try {
@@ -84,7 +83,6 @@ if(isset($_POST["created"])) {
         $question = $_POST["question"];
     }
     try {
-        require("kush.inc.php");
         $query = file_get_contents(__DIR__ . "/sql/queries/017_CREATE_TABLE_QUESTIONS.sql");
         if(isset($query) && !empty($query)) {
             $stmt = getDB()->prepare($query);
