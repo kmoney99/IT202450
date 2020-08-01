b104a3a92caf1ef63d0857764b35385d078911ee
Kush Patel <kp627@njit.edu>
2020-08-01 17:15:41 -0400

    fixed addquestion.php

--- public_html/list_surveys/add_question.php
+++ public_html/list_surveys/add_question.php
@@ -2,9 +2,9 @@
 include_once(__DIR__."/partials/header.partial.php");
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
+<?
 
 
-/*
 <form method="POST">
 
   <label for="question">Question:</label><br>
@@ -15,10 +15,9 @@ ini_set('display_errors', '1');
  <input type="submit" name="created" value="Save"/>
 
 </form>
-*/
 
 
-
+<?php
 require("common.inc.php");
 $query = file_get_contents(__DIR__ . "/sql/queries/SELECT_ALL_TABLE_SURVEY.sql");
 if(isset($query) && !empty($query)){
@@ -90,45 +89,3 @@ if(isset($_POST["created"])) {
     }
 }
 ?>
- <form method="POST">
-    <div class="list-group">
-        <?php foreach($available as $s): ?>
-            <div class="list-group-item">
-                <h4><?php echo Common::get($s[0], "question"); ?></h4>
-                <div class="list-group">
-                <?php foreach($s as $question):?>
-                <div class="list-group-item btn-group-toggle bg-light" data-toggle="buttons">
-                    <?php
-                    $id = "answer-".Common::get($question, "question_id") . "-" . Common::get($question, "answer_id");
-                    ?>
-                    <?php if(Common::get($question, "open_ended", false)):?>
-                        <label for="<?php echo $id;?>"><?php echo Common::get($question, "answer");?></label>
-                        <input id="<?php echo $id;?>" class="form-control" type="text"
-               name="question-<?php echo Common::get($question,"question_id", -1);?>-other-<?php echo Common::get($question, "answer_id", -1);?>" />
-                    <?php else:?>
-
-                        <label class="btn btn-secondary btn-lg btn-block" for="<?php echo $id;?>">
-                            <?php echo Common::get($question, "answer");?>
-                        <input id="<?php echo $id;?>" autocomplete="off" type="radio"
-                               name="question-<?php echo Common::get($question,"question_id", -1);?>"
-                                value="<?php echo Common::get($question, "answer_id", -1);?>"
-                                onclick="selectChoice(this);"/>
-                        </label>
-                    <?php endif; ?>
-                </div>
-                <?php endforeach;?>
-                </div>
-            </div>
-        <?php endforeach; ?>
-    </div>
-        <input type="submit" class="btn btn-primary" name="submit" value="Submit Response"/>
-    </form>
-</div>
-<script>
-    function selectChoice(ele){
-        $("[name=" + $(ele).attr("name") + "]").each(function(index, item){
-           $(item).closest("label").removeClass("active");
-        });
-        $(ele).closest("label").addClass("active");
-    }
-</script>
