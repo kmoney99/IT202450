f3f085ae8bb560c04cd023964f4d8bc7004cc935
Kush Patel <kp627@njit.edu>
2020-08-02 22:57:06 -0400

    added created by you

---
+++ public_html/list_surveys/listing.php
@@ -0,0 +1,28 @@
+<?php
+
+include_once(__DIR__."/partials/header.partial.php");
+require("common.inc.php");
+$query = file_get_contents(__DIR__ . "/queries/SELECT_ALL_TABLE_SURVEY.sql");
+if(isset($query) && !empty($query)){
+    try {
+        $stmt = getDB()->prepare($query);
+       
+        $stmt->execute();
+        
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+    catch (Exception $e){
+        echo $e->getMessage();
+    }
+}
+if(isset($results)) {
+	
+    echo "Surveys created by you";
+	
+       foreach($results as $row) {
+		
+            echo get($row, "title")
+	   }
+}
+
+?>
